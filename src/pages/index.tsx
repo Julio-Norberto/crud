7c63b6ea3272350375822044f2d4c889ece23425
Júlio César <juliocsscl@gmail.com>
2022-05-19 00:16:24 -0300

    create button novo cliente

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Botao from "../components/Botao";
 import Layout from "../components/Layout";
 import Tabela from "../components/Tabela";
 import Cliente from "../core/Cliente";
@@ -22,6 +23,10 @@ export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white">
         <Layout titulo="Cadastro simples">
+            <div className="flex justify-end">
+              <Botao className="mb-4">Novo Cliente</Botao>
+            </div>
+            
             <Tabela clientes={clientes} clienteSelecionado={clienteSelecionado} clienteExcluido={clienteExcluido} />
         </Layout>
     </div>
