211e57b7afd110af1bf7e0d88274671c44e900ab
Júlio César <juliocsscl@gmail.com>
2022-05-19 00:58:56 -0300

    create Entrada.tsx and Formulario.jsx

---
+++ src/components/Entrada.tsx
@@ -0,0 +1,25 @@
+interface EntradaProps {
+    tipo?: 'text' | 'number'
+    texto: string
+    valor: any
+    somenteLeitura?: boolean
+    valorMudou?: (valor: any) => void
+
+}
+
+export default function Entrada(props: EntradaProps) {
+    return (
+        <div className="flex flex-col mb-4">
+            <label className="mb-4">
+                {props.texto}
+            </label>
+            <input 
+                className={`border border-purple-500 rounded-lg focus:outline-none bg-gray-100 px-4 py-2 ${props.somenteLeitura ? '' : 'focus:bg-white'}`} 
+                type={props.tipo ?? 'text'} 
+                value={props.valor} 
+                readOnly={props.somenteLeitura}
+                onChange={e => props.valorMudou?.(e.target.value)} 
+            />
+        </div>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Botao from "../components/Botao";
+import Formulario from "../components/Formulario";
 import Layout from "../components/Layout";
 import Tabela from "../components/Tabela";
 import Cliente from "../core/Cliente";
@@ -27,7 +28,8 @@ export default function Home() {
               <Botao className="mb-4">Novo Cliente</Botao>
             </div>
             
-            <Tabela clientes={clientes} clienteSelecionado={clienteSelecionado} clienteExcluido={clienteExcluido} />
+            { /*<Tabela clientes={clientes} clienteSelecionado={clienteSelecionado} clienteExcluido={clienteExcluido} /> */ }
+            <Formulario cliente={clientes[2]} />
         </Layout>
     </div>
   )
